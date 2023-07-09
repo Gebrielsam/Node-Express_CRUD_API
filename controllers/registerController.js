6a995e12a0ea7664f81780b16887d85454109ff9
Onah Obinna Samuel <samgb042@gmail.com>
2023-07-09 11:08:34 +0100

    initial commit

---
+++ controllers/registerController.js
@@ -0,0 +1,28 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const {user, pwd} = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and Password are required.' });
+    // check for duplicate usernames in the DB
+    const duplicate = await User.findOne({ username: user }).exec();
+    if(duplicate) return res.status(409); //Conflict
+    try {
+        //encrypt password
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+
+        // create and store new user
+        const result = await User.create({ 
+            'username': user,
+            'password': hashedPwd
+    });
+
+    console.log(result);
+
+    res.status(201).json({ 'successs': `New user ${user} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
+}
+
+module.exports = { handleNewUser };
